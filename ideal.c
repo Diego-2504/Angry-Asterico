2417c546474a21bcf61ea94c4a9106db675e31f0
CElizabeth666 <77088489+CElizabeth666@users.noreply.github.com>
2021-01-12 00:29:03 -0600

    Update ideal.c

--- ideal.c
+++ ideal.c
@@ -1,8 +1,94 @@
 #include<stdio.h>
+#include<math.h>
 
+int NUMFILAS = 16;
+int NUMCOLS = 70;
+
+//función que inicializa loq ue se muestra en la pantalla
+void iniciapantalla( char pantalla[NUMFILAS][NUMCOLS])
+{
+ int i, j; 
+  for (i = 0; i < NUMFILAS; i++)
+  {
+    for (j = 0; j < NUMCOLS; j++)
+    { pantalla[i][j] = ' ' ; }
+  }
+ 
+  //dibujo de la diana
+  i=(int)NUMFILAS/2-1;
+  pantalla[i - 1][NUMCOLS - 1] = 'X'; 
+  pantalla[i][NUMCOLS - 1] = 'X'; 
+  pantalla[i + 1][NUMCOLS - 1] = 'X';
+  
+  //dibujo de cañón 
+  pantalla[NUMFILAS - 1][0] = '/'; 
+}
+//función que imprime la matríz en pantalla
+void dibujapantalla( char pantalla[NUMFILAS][NUMCOLS])
+{
+int i, j; 
+  printf("+++++++++++++++++++++++++++++++++++++++++++++++++\n");
+  
+  for( i = 0; i < NUMFILAS; i++)
+  {
+    for( j = 0; j < NUMFILAS; j++)
+    { printf("%c" , pantalla[i][j] ); }
+  }
+  printf("++++++++++++++++++++++++++++++++++++++++++++++++\n"); 
+}
+
+//función que calcula la posición del objeto
+void posicion( float v, float alfa, float t, float *px, float *py) 
+{
+  float g=9.8; 
+  float vx0 = v*cos(alfa*M_PI/180); 
+  float vy0 = v*sin(alfa*M_PI/180); 
+  
+  *px = vx0*t; 
+  *py = -(g*pow(t,2)/2) + vy0*t;
+}
 int main()
 {
-printf("Angry asterísco\n"); 
+  float x, y; //posición del objeto
+ //////////////////
+  float xd=100, yd=50; //posición de la diana 
+ /////////////////////// 
+  float alfa, v0, t; // ángulo, velocidad, tiempo 
+  int i, j, acierto =0; //contadores
+  char pantalla[NUMFILAS][NUMCOLS]; //matriz que dibuja la trayectoria del *
 
+  do
+  {
+    x = 0, y = 0; //inicializa el asterisco en el origen
+    iniciapantalla(pantalla);
+    dibujapantalla(pantalla);
+    
+    printf("Angry asterísco\n"); 
+    //El jugador introduce los datos
+    
+    printf("Introduce la velocidad inicial\n"); 
+    scanf("%f", &v0); 
+    printf("Introduce ahora el ángulo de disparo en grados \n"); 
+    scanf("%f", &alfa); 
+    
+    //loop que calcula la trayectoria
+    for( t=0; x<xd && x>=0; t=t+0.01)
+    {
+      posicion(v0, alfa, t, &x, &y); 
+      i=(int)(yd-y)/yd*NUMFILAS/2 + NUMFILAS/2 - 1; 
+      j = (int)x/xd*(NUMCOLS-1); 
+      
+      //impresión de * si está dentro de la pantalla
+      if ( i >= 0 && i<NUMFILAS && j >= 0 && j<NUMCOLS)
+      {
+       //Si se alcanza la diana se termina el loop
+        if(pantalla[i][j]=='X')
+        {acierto = 1; }
+        pantalla[i][j]='*';
+      }
+    }
+    dibujapantalla(pantalla);
+  } while (acierto ==0); 
+  printf("Lo lograste, pasas al siguiente nivel\n"); 
 return 0; 
 }
