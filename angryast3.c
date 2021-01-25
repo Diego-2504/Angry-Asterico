f38ebd912433679ef46eac84c3ae77d84768faed
vian2298 <69358313+vian2298@users.noreply.github.com>
2021-01-25 11:42:35 -0600

    Se agrego nivel 2

--- angryast3.c
+++ angryast3.c
@@ -1,7 +1,7 @@
 /////////////////////////////////////////////
 //Angry asterísco :
 //	Juego en el que se lanza un asterísco que debe alcanzar la X
-//Integrantes: Rosa Vianney
+//Integrantes: Rosa Vianney Avila 
 //	       Oscar Daniel Ortega
 //	       Diego Peña
 //	       Claudia Elizabeth Peña
@@ -122,6 +122,19 @@ void posicion( float v, float alfa, float t, float *px, float *py)
   *py = -(g*pow(t,2)/2) + vy0*t;
 }
 
+//función que calcula la posición del objeto
+//en un medio viscoso de parámetro "b"
+void posicionvisc( float v, float alfa, float t, float *px, float *py) 
+{
+  float g=9.8; 
+  float b=0.1;
+  float vx0 = v*cos(alfa*M_PI/180); 
+  float vy0 = v*sin(alfa*M_PI/180); 
+  
+  *px = (vx0/b)*(1-exp(-b*t)); 
+  *py = (1/b)*(g/b+vy0)*(1-exp(-b*t))-(g/b)*t;
+}
+
 //función que le da la oportunidad al usuario de ganar dos vidas extra
 
 void extravidas(void)
@@ -233,11 +246,78 @@ void nivel1(void)
   	} while (acierto ==0);
 }
 
+void nivel2(void)
+{
+  	float x, y; //posición del objeto
+ //////////////////
+ 	float xd=100, yd=50; //posición de la diana 
+ /////////////////////// 
+ 	float alfa, v0, t; // ángulo, velocidad, tiempo 
+ 	int i, j, acierto =0; //contadores
+ 	
+	////////
+	char pantalla[NUMFILAS][NUMCOLS]; //matriz que dibuja la trayectoria del * 	
+
+	do 
+ 	{ 
+	x = 0;
+	y = 0; //inicializa el asterisco en el origen
+	iniciapantalla(pantalla);
+    	dibujapantalla(pantalla);
+    
+   	printf("Angry asterísco\n"); 
+    	//El jugador introduce los datos
+	
+	printf("Introduce la velocidad inicial\n"); 
+    	scanf("%f", &v0); 
+    	printf("Introduce ahora el ángulo de disparo en grados \n"); 
+    	scanf("%f", &alfa); 
+    
+   	 //loop que calcula la trayectoria
+   	 for( t = 0; x < xd && x >= 0; t=t+0.01)
+    	{
+      	posicionvisc(v0, alfa, t, &x, &y); 
+      	i=(int)(yd-y)/yd*NUMFILAS/2 + NUMFILAS/2 - 1; 
+      	j = (int)x/xd*(NUMCOLS-1); 
+      
+      	//impresión de * si está dentro de la pantalla
+      	if ( i >= 0 && i < NUMFILAS && j >= 0 && j<NUMCOLS)
+      {
+       //Si se alcanza la diana se termina el loop
+	if(pantalla[i][j]=='X')
+        {acierto = 1; }	
+        pantalla[i][j]='*';
+      }
+    	}
+	  if(acierto == 0) //si no acierta
+	{  retiro(lifesi); //restamos una vida
+	   dibujapantalla(pantalla);//comienza de nuevo el juego
+	    	////////////////////////////////////////////
+		if(lifesi == 1)
+		{
+
+		extravidas();
+
+		 //dibujapantalla(pantalla);
+		}
+		//////////////////////////////////////////// 
+	  	if(lifesi == 0)//si se acaban las vidas
+		{
+		acierto = 1;  // Se termina el juego  
+		salir() ; } //despedida 
+		}
+	  else //Se logró el objetivo
+	{
+	dibujapantalla(pantalla); 
+	printf("Lo lograste!! Pasas al siguiente nivel \n"); }
+  	} while (acierto ==0);
+}
 
 int main()
 {
 
-	nivel1(); 	 
+	nivel1(); 
+  nivel2();	 
 
 return 0; 
 }
